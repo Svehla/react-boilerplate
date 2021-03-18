c474c5847c07793f7c448d71e5f2b44087a58629
Jakub Švehla <svehl.jakub@gmail.com>
2021-03-18 20:25:14 +0100

    fix ts build

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,10 +4,9 @@ module.exports = {
 
   parser: '@typescript-eslint/parser', // Specifies the ESLint parser
   extends: [
-    'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+    // 'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
     'plugin:react/recommended',
-    'prettier/@typescript-eslint', // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
-    // "prettier/react", // NOT SURE?! WORKING? disables react-specific linting rules that conflict with prettier
+    'prettier',
     'plugin:prettier/recommended', // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
   ],
   parserOptions: {
@@ -19,23 +18,24 @@ module.exports = {
   },
   rules: {
     semi: 'off',
-    '@typescript-eslint/semi': ['error', 'never'],
-    '@typescript-eslint/no-use-before-define': [
-      'error',
-      { functions: false, classes: false, variables: false, typedefs: true },
-    ],
-    '@typescript-eslint/explicit-function-return-type': 0,
-    '@typescript-eslint/prefer-interface': 0,
-    '@typescript-eslint/interface-name-prefix': 0,
-    '@typescript-eslint/no-non-null-assertion': 0,
-    '@typescript-eslint/camelcase': 0,
-    '@typescript-eslint/explicit-module-boundary-types': 0,
-    '@typescript-eslint/no-explicit-any': 0,
-    '@typescript-eslint/no-unused-vars': [1, { argsIgnorePattern: '^_' }],
-    'arrow-parens': ['error', 'as-needed'],
-    'no-use-before-define': ['error', { functions: false, classes: false, variables: false }],
-    'prefer-arrow-callback': 1,
+    // '@typescript-eslint/semi': ['error', 'never'],
+    // '@typescript-eslint/no-use-before-define': [
+    //   'error',
+    //   { functions: false, classes: false, variables: false, typedefs: true },
+    // ],
+    // '@typescript-eslint/explicit-function-return-type': 0,
+    // '@typescript-eslint/prefer-interface': 0,
+    // '@typescript-eslint/interface-name-prefix': 0,
+    // '@typescript-eslint/no-non-null-assertion': 0,
+    // '@typescript-eslint/camelcase': 0,
+    // '@typescript-eslint/explicit-module-boundary-types': 0,
+    // '@typescript-eslint/no-explicit-any': 0,
+    // '@typescript-eslint/no-unused-vars': [1, { argsIgnorePattern: '^_' }],
+    // 'arrow-parens': ['error', 'as-needed'],
+    // 'no-use-before-define': ['error', { functions: false, classes: false, variables: false }],
+    // 'prefer-arrow-callback': 1,
     '@typescript-eslint/ban-ts-ignore': 0,
+    'eslint-disable-next-line': 0,
     // because this rule is not working
     // "@typescript-eslint/explicit-member-accessibility": [ { accessibility: "no-public", overrides: { properties: "explicit" } } ],
     '@typescript-eslint/explicit-member-accessibility': 0,
