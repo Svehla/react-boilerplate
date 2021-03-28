b9c9dc9207de3431879cf208be503d1c824ca116
marek.hradil <marecek@marekhradil.net>
2021-03-28 17:03:43 +0200

    Design post form changes

--- src/pages/Posts.tsx
+++ src/pages/Posts.tsx
@@ -1,16 +1,10 @@
-import { Button, TextField } from '@material-ui/core'
+import { Card, CardContent, Grid } from '@material-ui/core'
 import { POSTS_FEED_DATA_FRAGMENT, PostsFeed } from '../components/PostsFeed'
-import { PageLoader } from '../components/PageLoader'
-import {
-  Posts_AddPostMutation,
-  Posts_AddPostMutationVariables,
-} from './__generated__/Posts_AddPostMutation'
 import { Posts_Query } from './__generated__/Posts_Query'
-import { Skeleton } from '@material-ui/lab'
 import { UserDetailContext } from '../globalState/UserDetailContext'
-import { appConfig } from '../appConfig'
-import { gql, useMutation, useQuery } from '@apollo/client'
-import { useContext, useState } from 'react'
+import { gql, useQuery } from '@apollo/client'
+import { useContext } from 'react'
+import CreatePostForm from '../components/CreatePostForm'
 
 export const POSTS_QUERY = gql`
   ${POSTS_FEED_DATA_FRAGMENT}
@@ -22,74 +16,25 @@ export const POSTS_QUERY = gql`
   }
 `
 
-export const ADD_POST_MUTATION = gql`
-  mutation Posts_AddPostMutation($input: addPostMutation_args_input_arg!) {
-    addPostMutation(input: $input) {
-      post {
-        id
-        text
-      }
-    }
-  }
-`
-
 export const Posts = () => {
-  const { data, loading, error, refetch } = useQuery<Posts_Query>(POSTS_QUERY)
-  const [postText, setPostText] = useState('')
+  const { data, loading, refetch } = useQuery<Posts_Query>(POSTS_QUERY)
   const userDetail = useContext(UserDetailContext)
 
-  const [addPost] = useMutation<Posts_AddPostMutation, Posts_AddPostMutationVariables>(
-    ADD_POST_MUTATION
-  )
-
   return (
-    <div style={{ marginTop: '2rem' }}>
-      {loading ? (
-        <Skeleton variant='rect' width={'100%'} height={47} />
-      ) : userDetail.data?.isViewerLoggedIn ? (
-        <div>
-          <form
-            onSubmit={async e => {
-              e.preventDefault()
-
-              try {
-                await addPost({
-                  variables: {
-                    input: {
-                      text: postText,
-                    },
-                  },
-                })
-
-                setPostText('')
-
-                refetch()
-              } catch (err) {
-                alert(JSON.stringify(err.message))
-              }
-            }}
-          >
-            <TextField
-              label='Přidat post'
-              onChange={e => {
-                setPostText(e.target.value)
-              }}
-              value={postText}
-              type='text'
-              disabled={loading}
+    <Grid container spacing={3}>
+      <Grid item xs={8}>
+        <Card>
+          <CardContent>
+            <CreatePostForm
+              loading={loading}
+              isLogged={userDetail?.data?.isViewerLoggedIn ?? false}
             />
-
-            <Button type='submit'>odeslat</Button>
-          </form>
-        </div>
-      ) : (
-        <h1>
-          Chceš napsat komentář?
-          <a href={appConfig.google.authLoginURL}>TAK SE PŘIHLAŠ!</a>
-        </h1>
-      )}
-
-      <PostsFeed loading={loading} data={data?.posts} />
-    </div>
+          </CardContent>
+        </Card>
+      </Grid>
+      <Grid item xs={12}>
+        <PostsFeed loading={loading} data={data?.posts} />
+      </Grid>
+    </Grid>
   )
 }
